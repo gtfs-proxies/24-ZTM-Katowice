1e8a819cc09e212dfa495d4c5c39a03427570b61
Artur H. Lange <artur@arc2.dev>
2024-03-30 14:46:19 +0100

    Fix unzip location

--- unzip.sh
+++ unzip.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 FILE_LOCATION=/tmp/$FEED_NAME/original
-FEED_FILE=$(ls $FILE_LOCATION)
+FEED_FILE=$(ls $FILE_LOCATION | sort -r | head -1)
 
 unzip -u $FILE_LOCATION/$FEED_FILE -d feed/
