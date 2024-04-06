6ae181e967d1cce1bcaca5c2143e249680591b9b
Artur H. Lange <artur@arc2.dev>
2024-04-06 12:50:11 +0200

    Fix unzipping

--- unzip.sh
+++ unzip.sh
@@ -3,4 +3,4 @@
 FILE_LOCATION=/tmp/$FEED_NAME/original
 FEED_FILE=$(ls $FILE_LOCATION | sort -r | head -1)
 
-unzip -u $FILE_LOCATION/$FEED_FILE -d feed/
+unzip -o $FILE_LOCATION/$FEED_FILE -d feed/
