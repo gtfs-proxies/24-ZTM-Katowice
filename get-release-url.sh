6dec4937fb9f2f3929db15253afe979bb3ef50c9
Olaf Kryus <14991562+olafkryus@users.noreply.github.com>
2025-02-23 12:13:15 +0100

    Add home.pl SSL cert file

--- get-release-url.sh
+++ get-release-url.sh
@@ -4,12 +4,13 @@
 # get URL to download latest GTFS feed
 #
 
-RELEASE_URL=$(curl --connect-timeout 30 -s https://otwartedane.metropoliagzm.pl/dataset/317435cc-0075-4d10-b8ef-6e9b0010e90a.jsonld | \
-              jq ' ."@graph"[]."dcat:accessURL"."@id"'                                                                              | \
-              grep -F 'schedule_ztm'                                                                                                | \
-              sed -e 's/^"//' -e 's/"$//' -e 's/^\(.*\)\(schedule_ztm.*\)$/\2 \1/'                                                  | \
-              sort -r                                                                                                               | \
-              head -1                                                                                                               | \
+DATASET_URL="https://otwartedane.metropoliagzm.pl/dataset/317435cc-0075-4d10-b8ef-6e9b0010e90a.jsonld"
+RELEASE_URL=$(curl --connect-timeout 30 --cacert ./home-pl.pem -s $DATASET_URL     | \
+              jq ' ."@graph"[]."dcat:accessURL"."@id"'                             | \
+              grep -F 'schedule_ztm'                                               | \
+              sed -e 's/^"//' -e 's/"$//' -e 's/^\(.*\)\(schedule_ztm.*\)$/\2 \1/' | \
+              sort -r                                                              | \
+              head -1                                                              | \
               sed -e 's/^\(.*\) \(http.*\)$/\2\1/')
 
 if [ -n "$RELEASE_URL" ]
