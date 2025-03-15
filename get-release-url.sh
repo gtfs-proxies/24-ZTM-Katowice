b7c8d4564233d79ae93f470a1b69b23bfd90e059
Artur H. Lange <artur@arc2.dev>
2025-03-15 12:01:45 +0100

    Change URL to simple dataset

--- get-release-url.sh
+++ get-release-url.sh
@@ -4,12 +4,18 @@
 # get URL to download latest GTFS feed
 #
 
-RELEASE_URL=$(curl --connect-timeout 30 -sk https://otwartedane.metropoliagzm.pl/dataset/317435cc-0075-4d10-b8ef-6e9b0010e90a.jsonld | \
-              jq ' ."@graph"[]."dcat:accessURL"."@id"'                                                                              | \
-              grep -F 'schedule_ztm'                                                                                                | \
-              sed -e 's/^"//' -e 's/"$//' -e 's/^\(.*\)\(schedule_ztm.*\)$/\2 \1/'                                                  | \
-              sort -r                                                                                                               | \
-              head -1                                                                                                               | \
+# Extended dataset, GTFS feeds are exclusive e.g. for one day, can be user after merging.
+# DATASET_URL="https://otwartedane.metropoliagzm.pl/dataset/317435cc-0075-4d10-b8ef-6e9b0010e90a.jsonld
+
+# Simple dataset, can be used as is
+DATASET_URL="https://otwartedane.metropoliagzm.pl/dataset/5d8d7145-1be1-4ed2-9c18-5535e056a56d.jsonld
+
+RELEASE_URL=$(curl --connect-timeout 30 -sk $DATASET_URL                            | \
+              jq ' ."@graph"[]."dcat:accessURL"."@id"'                              | \
+              grep -F 'schedule_ztm'                                                | \
+              sed -e 's/^"//' -e 's/"$//' -e 's/^\(.*\)\(schedule_ztm.*\)$/\2 \1/'  | \
+              sort -r                                                               | \
+              head -1                                                               | \
               sed -e 's/^\(.*\) \(http.*\)$/\2\1/')
 
 if [ -n "$RELEASE_URL" ]
