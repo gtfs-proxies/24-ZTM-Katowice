7da697c0b8e173edbf70fbe329157c70a74d6e43
Artur H. Lange <artur@arc2.dev>
2026-02-06 17:09:07 +0100

    Add incremental merging

--- gtfsmerge.py
+++ gtfsmerge.py
@@ -1,47 +1,23 @@
 #!/usr/bin/env python3
 
-"""Script to merge GTFS ZIP archives into one.
-
-Usage: ./gtfsmerge.py gtfs1.zip gtfs2.zip gtfs3.zip output.zip
-
-Features:
-    * Uses the first archive contents as a reference.
-    * Supports wildcards in input argumets.
-    * Skips files from other archives with a different header.
-    * Adds the CSV header row once per file in the output archive.
-    * Avoids lines with duplicate indexes.
-
-Note that the script doesn't check the input or output CSV files validity
-nor GTFS compliance.
-"""
-
 import glob
 import logging
 import sys
 import zipfile
-from typing import Dict, List, Set
 
-__author__ = "m0wer"
-__copyright__ = "Copyright 2021, m0wer"
-__license__ = "GPLv3"
-__version__ = "1.2.0"
-__maintainer__ = "m0wer"
-__email__ = "m0wer@autistici.org"
-__status__ = "Production"
-
-FILE_INDEXES: Dict[str, List[str]] = {
-    "agency.txt": ["agency_id"],
-    "calendar.txt": ["service_id", "start_date", "end_date"],
-    "calendar_dates.txt": ["service_id", "date"],
-    "fare_attributes.txt": ["fare_id"],
-    "fare_rules.txt": ["fare_id"],
-    "feed_info.txt": ["feed_publisher_name"],
-    "frequencies.txt": ["trip_id", "start_time"],
-    "routes.txt": ["route_id"],
-    "shapes.txt": ["shape_id", "shape_pt_sequence"],
-    "stop_times.txt": ["trip_id", "stop_sequence"],
-    "stops.txt": ["stop_id"],
-    "trips.txt": ["trip_id"],
+FILE_INDEXES: dict[str, set[str]] = {
+    "agency.txt": {"agency_id"},
+    "calendar.txt": {"service_id", "start_date", "end_date"},
+    "calendar_dates.txt": {"service_id", "date"},
+    "fare_attributes.txt": {"fare_id"},
+    "fare_rules.txt": {"fare_id"},
+    "feed_info.txt": {"feed_publisher_name"},
+    "frequencies.txt": {"trip_id", "start_time"},
+    "routes.txt": {"route_id"},
+    "shapes.txt": {"shape_id", "shape_pt_sequence"},
+    "stop_times.txt": {"trip_id", "stop_sequence"},
+    "stops.txt": {"stop_id"},
+    "trips.txt": {"trip_id"},
 }
 
 
@@ -50,10 +26,11 @@ logging.basicConfig(
     level=logging.ERROR,
 )
 
+ENCODING = "utf-8-sig"
 
 def main():
     """Run the program."""
-    gtfs_archive_paths: str = [
+    gtfs_archive_paths: list[str] = [
         path for arg in sys.argv[1:-1] for path in glob.glob(arg)
     ]
     output_path: str = sys.argv[-1]
@@ -70,7 +47,7 @@ def main():
             logging.info("Processing %s...", gtfs_file)
 
             seen_lines: set[bytes] = set()
-            seen_ids: set[tuple] = set()
+            seen_ids: set[tuple[str, ...]] = set()
 
             # open a file with the same name in the resulting zip
             with result.open(gtfs_file, "w") as result_gtfs_file:
@@ -80,10 +57,10 @@ def main():
                     gtfs_file
                 ) as reference_gtfs_file:
                     logging.info("\tWriting header from reference...")
-                    header: str = reference_gtfs_file.readline()
+                    header: bytes = reference_gtfs_file.readline()
                     result_gtfs_file.write(header)
-                    logging.debug("\t%s", header.decode("utf-8-sig"))
-                non_duplicable_indexes_index: List[int] = []
+                    logging.debug("\t%s", header.decode(ENCODING))
+                non_duplicable_indexes_index: list[int] = []
 
                 if gtfs_file not in FILE_INDEXES:
                     logging.warning("\t\tUsing first column as index.")
@@ -91,7 +68,7 @@ def main():
                 else:
                     for index in FILE_INDEXES[gtfs_file]:
                         non_duplicable_indexes_index.append(
-                            header.decode("utf-8-sig")
+                            header.decode(ENCODING)
                             .replace("\r\n", "")
                             .split(",")
                             .index(index)
@@ -106,8 +83,8 @@ def main():
                     ) as content:
                         if content.readline() == header:
                             for line in content:
-                                index_tuple: tuple = tuple(
-                                    line.decode("utf-8-sig")
+                                index_tuple: tuple[str, ...] = tuple(
+                                    line.decode(ENCODING)
                                     .replace("\r\n", "")
                                     .split(",")[index]
                                     for index in non_duplicable_indexes_index
@@ -121,13 +98,13 @@ def main():
                                         logging.debug(
                                             "\t\tAvoiding exact row duplicate"
                                             ": %s",
-                                            line.decode("utf-8-sig"),
+                                            line.decode(ENCODING),
                                         )
                                     else:
                                         logging.info(
                                             "\t\tAvoiding row with duplicate "
                                             "IDs: %s",
-                                            line.decode("utf-8-sig"),
+                                            line.decode(ENCODING),
                                         )
 
                         else:
